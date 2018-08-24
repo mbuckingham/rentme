20345726438842c4d6f93961bdd6013e448ed0b7
Matt <mbuckingham@umassd.edu>
2018-08-24 00:59:03 -0400

    Added room controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
+
   devise_for :users,
               path: '',
               path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
               controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
 
-resources :users, only: [:show]
+  resources :users, only: [:show]
+  resources :rooms, only: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
 end
