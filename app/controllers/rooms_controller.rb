e817bbafd488d2e17dbf3732c6ce9e0e03f1a8e9
Matt <mbuckingham@umassd.edu>
2018-08-26 17:14:54 -0400

    added photo controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,7 @@
 class RoomsController < ApplicationController
   before_action :set_room, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:show]
+  before_action :is_authorised, only: [:listing, :pricing, :description, :photo_upload, :amenities, :location, :update]
 
   def index
     @rooms = current_user.rooms
@@ -32,6 +33,7 @@ class RoomsController < ApplicationController
   end
 
   def photo_upload
+    @photos = @room.photos
   end
 
   def amenities
@@ -52,6 +54,10 @@ class RoomsController < ApplicationController
 
   def set_room
     @room = Room.find(params[:id])
+  end
+
+  def is_authorised
+    redirect_to root_path, alert: "You don't have permission" unless current_user.id == @room.user_id
   end
 
   def room_params
